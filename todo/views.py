a486bf11661264c4c9a8d97a76c7b526f9aac0b5
joannawojtasz <joannawojtasz@gmail.com>
2021-10-16 15:03:28 +0000

    todo app

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render, HttpResponse
+from .models import Item
 
 # Create your views here.
+
+
 def get_todo_list(request):
+    items = Item.objects.all()
+    context = {
+        'items' : items
+    }
     return render(request, 'todo/todo_list.html')
+
+
+def add_item(request):
+    return render(request, 'todo/add_item.html')
